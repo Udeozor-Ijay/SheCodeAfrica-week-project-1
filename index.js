86646baec5754e1d9c372c04f03cd5a9d75d61bb
Udeozor Ebube <92553461+Udeozor-Ijay@users.noreply.github.com>
2022-08-15 22:30:26 +0100

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+
+const harmburger = document.querySelector(".harmburger");
+const navMenu = document.querySelector(".nav-menu");
+
+harmburger.addEventListener("click", () => {
+    harmburger.classList.Toggle("active");
+    navMenu.classList.Toggle("active");
+})
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
